6fee6bfb7ddcdd71811e646c8166c33efb8691f6
malogalbo <mlogalbo@ufl.edu>
2023-04-03 22:40:12 -0400

    third commit

--- Function.cs
+++ Function.cs
@@ -1,5 +1,7 @@
 using Amazon.Lambda.Core;
 using Newtonsoft.Json;
+using System.IO;
+using System;
 using System.Text;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
@@ -9,7 +11,7 @@ namespace EsepWebhook;
 
 public class Function
 {
-    
+
     /// <summary>
     /// A simple function that takes a string and does a ToUpper
     /// </summary>
@@ -18,20 +20,20 @@ public class Function
     /// <returns></returns>
     public string FunctionHandler(string input, ILambdaContext context)
     {
-        dynamic json = JsonConvert.DeserializeObject<dynamic>(input.ToString());
-		
-		string payload = $"{{'text':'Issue Created: {json.issue.html_url}'}}";
-		
-		var client = new HttpClient();
-		var webRequest = new HttpRequestMessage(HttpMethod.Post, Environment.GetEnvironmentVariable("SLACK_URL"))
-		{
-			Content = new StringContent(payload, Encoding.UTF8, "application/json")
-		};
-		
-		var response = client.Send(webRequest);
-		using var reader = new StreamReader(response.Content.ReadAsStream());
-		
-		return reader.ReadToEnd();
-		///return input.ToUpper();
+        dynamic json = JsonConvert.DeserializeObject<dynamic>(JsonConvert.SerializeObject(input));
+
+        string payload = $"{{'text':'Issue Created: {json.issue.html_url}'}}";
+
+        var client = new HttpClient();
+        var webRequest = new HttpRequestMessage(HttpMethod.Post, Environment.GetEnvironmentVariable("SLACK_URL"))
+        {
+            Content = new StringContent(payload, Encoding.UTF8, "application/json")
+        };
+
+        var response = client.Send(webRequest);
+        using var reader = new StreamReader(response.Content.ReadAsStream());
+
+        return reader.ReadToEnd();
+        ///return input.ToUpper();
     }
 }
